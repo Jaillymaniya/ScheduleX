3c68b40cbeee11bda4cdd4ae214aa9c3f8e56ce1
jaillymaniya <jelimaniya@gmail.com>
2026-03-26 18:22:53 +0530

    Implement timetable basic configuration

---
+++ ScheduleX.Web/DTOs/BreakRuleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ScheduleX.Web.DTOs;
+
+public class BreakRuleDto
+{
+    public int BreakRuleId { get; set; }
+    public int ConfigId { get; set; }
+    public byte BreakNo { get; set; }
+    public byte AfterLectureNo { get; set; }
+    public int BreakDurationMin { get; set; }
+    public string? BreakName { get; set; }
+}
---
+++ ScheduleX.Web/DTOs/TemplateDto.cs
@@ -0,0 +1,11 @@
+﻿using ScheduleX.Core.Entities;
+
+namespace ScheduleX.Web.DTOs;
+
+public class TemplateDto
+{
+    public int TemplateId { get; set; }
+    public string TemplateName { get; set; } = "";
+    public LayoutTypeEnum LayoutType { get; set; }
+    public bool IsDefault { get; set; }
+}
